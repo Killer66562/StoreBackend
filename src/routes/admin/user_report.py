ab2e337df357ab4b2fffcf13c25fb46a38ebe76d
Killer66562 <jimmy688382@gmail.com>
2024-04-01 12:00:26 +0800

    Add get solo item

--- src/routes/admin/user_report.py
+++ src/routes/admin/user_report.py
@@ -22,4 +22,11 @@ def get_all_user_reports(db: Session = Depends(get_db)):
     user_reports_query = db.query(UserReport)
     return paginate(user_reports_query)
 
+@router.get("/{user_report_id}", response_model=UserRepoertSchema)
+def get_all_user_reports(user_report_id: int, db: Session = Depends(get_db)):
+    report = db.query(UserReport).filter(UserReport.id == user_report_id).first()
+    if not report:
+        return JSONResponse(content={"message": "資源不存在"}, status_code=400)
+    return report
+
 add_pagination(router)
