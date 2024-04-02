8c12d312ac3c894734428c9dcfa7d7e0b6739e27
Killer66562 <jimmy688382@gmail.com>
2024-04-02 20:43:58 +0800

    Add more about item route

--- src/routes/general/item.py
+++ src/routes/general/item.py
@@ -1,8 +1,9 @@
+import random
 from fastapi.responses import JSONResponse
 from fastapi.routing import APIRouter
 from fastapi import Depends, Response
 
-from fastapi_pagination import Page
+from fastapi_pagination import Page, add_pagination
 from fastapi_pagination.ext.sqlalchemy import paginate
 
 from sqlalchemy.orm import Session
@@ -20,9 +21,27 @@ router = APIRouter(prefix="/items")
 def get_items(db: Session = Depends(get_db)):
     return paginate(db.query(Item))
 
+@router.get("/hot", response_model=list[FullItemSchema], status_code=200)
+def get_items(db: Session = Depends(get_db)):
+    hot_items_raw = db.query(Item).order_by(Item.id).limit(50).all()
+    hot_items_raw_len = len(hot_items_raw)
+    rand_k = 20 if hot_items_raw_len >= 20 else hot_items_raw_len
+    hot_items = random.sample(population=hot_items_raw, k=rand_k)
+    return sorted(hot_items, key=lambda item : item.id, reverse=False)
+
+@router.get("/good", response_model=list[FullItemSchema], status_code=200)
+def get_items(db: Session = Depends(get_db)):
+    good_items_raw = db.query(Item).order_by(Item.id).limit(50).all()
+    good_items_raw_len = len(good_items_raw)
+    rand_k = 20 if good_items_raw_len >= 20 else good_items_raw_len
+    good_items = random.sample(population=good_items_raw, k=rand_k)
+    return sorted(good_items, key=lambda item : item.id, reverse=True)
+
 @router.get("/{item_id}", response_model=FullItemSchema)
 def get_specific_item(item_id: int, db: Session = Depends(get_db)):
     item = db.query(Item).filter(Item.id == item_id).first()
     if not item:
         return JSONResponse(content={"message": "資源不存在"}, status_code=404)
     return item
+
+add_pagination(router)
