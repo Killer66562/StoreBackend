4105a972eb766a65c079da16e49fb0e15c58e218
Killer66562 <jimmy688382@gmail.com>
2024-04-02 15:19:23 +0800

    Add item_report route

--- src/routes/admin/root.py
+++ src/routes/admin/root.py
@@ -1,9 +1,11 @@
 from fastapi.routing import APIRouter
 
-from . import district, city, user_report
+from . import district, city, user_report, item_report
 
 
 router = APIRouter(prefix="/admin")
+
 router.include_router(district.router)
 router.include_router(city.router)
 router.include_router(user_report.router)
+router.include_router(item_report.router)
--- src/schemas/admin.py
+++ src/schemas/admin.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from .general import BaseResourceSchema, UserSchema
+from .general import BaseResourceSchema, UserSchema, ItemSchema
 
 
 class CUCitySchema(BaseModel):
@@ -22,3 +22,16 @@ class UserRepoertSchema(BaseResourceSchema):
     reporter: UserSchema
     reported_user: UserSchema
     images: list[UserRepoertImageSchema]
+
+
+class ItemRepoertImageSchema(BaseResourceSchema):
+    report_id: int
+    path: str
+
+
+class ItemReportSchema(BaseResourceSchema):
+    reporter_id: int
+    reported_item_id: int
+    reporter: UserSchema
+    reported_item: ItemSchema
+    images: list[ItemRepoertImageSchema]
