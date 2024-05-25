961758b5b0a241c3c43a555bbeff913f553e2d6f
Killer66562 <jimmy688382@gmail.com>
2024-05-25 21:45:12 +0800

    Fix route name

--- src/routes/admin/item_report.py
+++ src/routes/admin/item_report.py
@@ -14,22 +14,22 @@ from sqlalchemy.orm import Session
 from models import User, ItemReport
 
 
-router = APIRouter(prefix="/user_reports")
+router = APIRouter(prefix="/item_reports")
 
 @router.get("", response_model=Page[ItemReportSchema])
-def get_all_user_reports(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+def get_all_item_reports(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     item_reports_query = db.query(ItemReport)
     return paginate(item_reports_query)
 
 @router.get("/{item_report_id}", response_model=ItemReportSchema)
-def get_user_report(item_report_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+def get_item_report(item_report_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     report = db.query(ItemReport).filter(ItemReport.id == item_report_id).first()
     if not report:
         return JSONResponse(content={"message": "資源不存在"}, status_code=400)
     return report
 
 @router.delete("/{item_report_id}")
-def delete_user_report(item_report_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+def delete_item_report(item_report_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     report = db.query(ItemReport).filter(ItemReport.id == item_report_id).first()
     if not report:
         return JSONResponse(content={"message": "資源不存在"}, status_code=400)
